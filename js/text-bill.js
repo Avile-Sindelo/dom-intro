0ef8f00438062d47b2e595e112accdf942c322d4
Avile Sindelo <avilesindelo@gmail.com>
2023-04-18 13:32:12 +0200

    text-bill finished

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,32 +1,74 @@
+/***
+ * ******* PSUEDOCODE *******
+ * 
+ *  1. Get reference to the input element
+ *  2. Get reference to the Add button
+ *  3. Attach an event listener to the button
+ *  4. Create a variable for the new additional cost
+ *  5. If the input service is a call
+ *      5.1. Get reference to the call total element AND add the new additional cost to the calls total 
+ *          Display the new call total on the element
+ *      5.2. Get reference to the Global total element AND add the new additional costs to the global total
+ *          Display the new global total on the UI
+ *          Clear the text field
+ * 
+ *  6. If the input is an SMS
+ *      6.1. Get the reference to the SMS total element AND add the new additional cost to the SMS total
+ *          Display the new SMS total on element
+ *      6.2. Get reference to the Global total element AND add the new additional costs to the global total
+ *          Display the new global total on the UI
+ *          Clear the text box
+ * 
+ * ********* THE END ************
+ */
+
+
 // get a reference to the textbox where the bill type is to be entered
 const billTypeElement = document.querySelector('.billTypeText');
 
 //get a reference to the add button
 const addBtn = document.querySelector('.addToBillBtn');
 
-//create a variable that will keep track of the total bill
-let totalBill = 0;
-
-let callTotal = document.querySelector('.callTotalOne').innerHTML;
-let smsTotal = document.querySelector('.smsTotalOne').innerHTML;
+//create a variable that will keep track of the additional cost
+let additionalCost = 0;
 
 //add an event listener for when the add button is pressed
 addBtn.addEventListener('click', function(){
-//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-    if(billTypeElement.value == 'sms'){
-        // * add the appropriate value to the running total
-        totalBill += 0.75;
-        smsTotal += totalBill;
-    } else if(billTypeElement.value == 'call'){
-        totalBill += 2.75;
-        callTotal += totalBill;
-    } else {
-        // * add nothing for invalid values that is not 'call' or 'sms'.
-        totalBill += 0;
+    if(billTypeElement.value == 'call'){
+        //Additional cost for each call
+        additionalCost = 2.75;
+        //Get reference to the CALLS total 
+        const callTotal = document.querySelector('.callTotalOne');
+        //Convert the string total into a floating point number and add the additional cost
+        let updatedCallTotal = parseFloat(callTotal.innerHTML) + additionalCost;
+        //Display the updated total on the CALLS total
+        document.querySelector('.callTotalOne').innerHTML = updatedCallTotal;
+        //Get reference to the global total element
+        const globalTotal = document.querySelector('.totalOne');
+        //Convert the string value of the global total and add to it the additional cost
+        let updatedGlobalTotal = parseFloat(globalTotal.innerHTML) + additionalCost;
+        //Display the updated Global total
+        document.querySelector('.totalOne').innerHTML = updatedGlobalTotal;
+        //Clear the input field
+        billTypeElement.value = '';
+    } else if(billTypeElement.value == 'sms'){
+        //Additional cost for each call
+        additionalCost = 0.75;
+        //Get reference to the SMS total 
+        const smsTotal = document.querySelector('.smsTotalOne');
+        //Convert the string total into a floating point number and add the additional cost
+        let updatedSmsTotal = parseFloat(smsTotal.innerHTML) + additionalCost;
+        //Display the updated total on the CALLS total
+        document.querySelector('.smsTotalOne').innerHTML = updatedSmsTotal;
+        //Get reference to the global total element
+        const globalTotal = document.querySelector('.totalOne');
+        //Convert the string value of the global total and add to it the additional cost
+        let updatedGlobalTotal = parseFloat(globalTotal.innerHTML) + additionalCost;
+        //Display the updated Global total
+        document.querySelector('.totalOne').innerHTML = updatedGlobalTotal;
+        //Clear the input field
+        billTypeElement.value = '';
     }
-
-
-    // * display the latest total on the screen
-    
-    
 });
+
+    
